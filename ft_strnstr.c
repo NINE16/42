0266a256f209dd76d9de0cdbf36289036443d01b
NINE16 <74472310+NINE16@users.noreply.github.com>
2021-11-28 17:48:40 +0100

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nnemeth <nnemeth@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 11:53:12 by nnemeth           #+#    #+#             */
-/*   Updated: 2021/11/11 18:23:11 by nnemeth          ###   ########.fr       */
+/*   Updated: 2021/11/28 17:44:17 by nnemeth          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,28 @@ char	*ft_strnstr(const char *big, const char *small, size_t len)
 	size_t	j;
 
 	i = 0;
-	j = 0;
-	if (small[j] != '\0')
-	{
-		while (big[i] && i < len)
-		{
-			if (big[i] != small[j])
-			{
-				i++;
-				j = 0;
-			}
-			if ((big[i] == small[j]) && (i < len))
-			{
-				i++;
-				j++;
-				return ((char *)(big + i));
-			}
-		}
-	}
-	if (small[j] == '\0')
-		return ((char *)big);
-	else
+	if (!small || small[0] == '\0')
+		return ((char *) big);
+	if (!big || !small)
 		return (0);
+	while (big[i] && i < len)
+	{	
+		j = 0;
+		while (big[i + j] && small[j] && (i + j < len) && big[i + j] == small[j])
+			j++;
+		if (!small[j])
+			return ((char *)(big + i));
+		i++;
+	}
+	return (NULL);
 }
-/*int main ()
-{
-	char* s1 = "see FF your FF return FF now FF";
-	char* s2 = "FF";
-	size_t size = 20;
 
-	printf("%s\n", ft_strnstr(s1,s2,size));
-	printf("%s", strnstr(s1,s2,size));
-}*/
+// int main ()
+// {
+// 	char* s1 = "see FF your FF return FF now FF";
+// 	char* s2 = "FF";
+// 	size_t size = -1;
+
+// 	printf("%s\n", ft_strnstr(s1,s2,size));
+// 	printf("%s", strnstr(s1,s2,size));
+// }
